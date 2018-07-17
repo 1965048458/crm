bdf2b37a5c37f8b36a97165979e8b336ca05a04c
徐天赐 <1342532052@qq.com>
2018-07-17 15:48:46 +0800

    增加日志测试代码

--- src/test/java/com/xuebei/crm/journal/JournalControllerTest.java
+++ src/test/java/com/xuebei/crm/journal/JournalControllerTest.java
@@ -1,5 +1,6 @@
 package com.xuebei.crm.journal;
 
+import com.xuebei.crm.user.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,10 +11,13 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.Mockito.timeout;
-import static org.mockito.Mockito.verify;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -29,24 +33,51 @@ public class JournalControllerTest {
     @Mock
     private JournalService journalService;
 
-    private MockMvc mockMvc;
+
+    MockMvc mockMvc;
 
     @Before
-    public void setup () {
+    public void setup(){
         mockMvc = MockMvcBuilders.standaloneSetup(journalController).build();
     }
 
-//    @Test
-//    public void testSearchJournal() throws Exception {
-//        String keyword = "day";
-//        String isRead = "on";
-//        mockMvc.perform(get("/journal/search")
-//                .param("keyword", keyword)
-//                .param("isRead", isRead))
-//                .andExpect(view().name("searchRlt"))
-//                .andExpect(model().attributeExists("journalList"));
-//
-//        verify(journalService).searchJournal(keyword);
-//    }
+    @Test
+    public void testDetail() throws Exception{
+
+        String journalId = "123144";
+        String url = "/journal/detail";
+        User user = new User();
+        user.setUserId("547568");
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        User nUser = new User();
+        nUser.setUserId("96465");
+        List<User> nUserList = new ArrayList<>();
+        nUserList.add(nUser);
+
+        Journal journal = new Journal();
+        journal.setJournalId("123144");
+        journal.setUserId("463465");
+        List<Journal> journalList = new ArrayList<>();
+        journalList.add(journal);
+
+        List result = new ArrayList();
+
+        result.add(journalList);
+        result.add(userList);
+        result.add(nUserList);
+
+        when(journalService.searchDatail(journalId)).thenReturn(result);
+
+        mockMvc.perform(get(url).param("journalId", journalId))
+                .andExpect(jsonPath("successFlg").value(true))
+                .andExpect(jsonPath("journal[0].userId").exists())
+                .andExpect(jsonPath("unread[0].userId").exists())
+                .andExpect(jsonPath("read[0].userId").exists());
+
+        verify(journalService).searchDatail(journalId);
+
+    }
+
 
 }
