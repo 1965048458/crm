141d0b36a9a23fb30da139f6628114b4539603fd
郭培林 <1558047230@qq.com>
2018-07-19 11:26:10 +0800

    组织机构前端

--- src/main/resources/static/organization/showOrganization.js
+++ src/main/resources/static/organization/showOrganization.js
@@ -38,13 +38,61 @@ var customerList = [
     }
 ];
 jQuery(document).ready(function () {
-
+    new Vue({
+        el: '#organizationVue',
+        data: {
+            showCustomerOrganization:true,
+            showOrganization: false,
+            showApplyDialog: false,
+            showApply: false,
+            customerList: ''
+            //data: ''
+        },
+        methods: {
+            'searchOrganizations': function () {
+                var thisVue = this;
+                jQuery.ajax({
+                    type: 'get',
+                    url: '/journal/list',
+                    data: '',
+                    dataType: 'json',
+                    cache: false
+                }).done(function () {
+                    thisVue.showOrganization = true;
+                    thisVue.$set(thisVue, 'customerList', customerList);
+                });
+            },
+            'apply':function () {
+                this.showApplyDialog=true
+            },
+            'dialogCheck': function () {
+                this.showApplyDialog = false;
+                this.showCustomerOrganization = false;
+                this.showApply = true
+            },
+            'dialogQuit': function () {
+                this.showApplyDialog = false;
+            },
+            'applySubmit':function () {
+                this.showApply=false;
+                this.showCustomerOrganization=true;
+            },
+            'applyQuit':function () {
+                this.showApply=false;
+                this.showCustomerOrganization=true;
+            }
+        }
+    });
     Vue.component('customer', {
         template: '#customer',
         props: ['customer'],
         data: function () {
             return {
-                showSub: false
+                showSub: false,
+                showCustomerOrganization:true,
+                showOrganization: false,
+                showApplyDialog: false,
+                showApply: false,
             };
         },
         methods: {
@@ -66,38 +114,12 @@ jQuery(document).ready(function () {
                 else{
                     return "";
                 }
-            }
-        }
-    });
-    var showOrganizationVue = new Vue({
-        el: '#organizationVue',
-        data: {
-            showOrganization: false,
-            showApplyDialog:false,
-            showApply:false,
-            customerList: '',
-            data: ''
-        },
-        methods: {
-            'searchOrganizations': function () {
-                var thisVue = this;
-                jQuery.ajax({
-                    type: 'get',
-                    url: '/journal/list',
-                    data: '',
-                    dataType: 'json',
-                    cache: false
-                }).done(function () {
-                    thisVue.showOrganization = true;
-                    thisVue.$set(thisVue, 'customerList', customerList);
-                });
             },
-            'applyCheck':function(){
-                
+            'apply':function (name) {
+                this.showApplyDialog = true;
+                console.log(name);
             },
-            'applyQuit':function () {
-                
-            }
+
         }
     });
 });
