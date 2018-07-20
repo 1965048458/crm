d1154a1bd5fbc3c66f5bfb627a7b5e121b08b2fc
徐天赐 <1342532052@qq.com>
2018-07-20 12:57:31 +0800

    增加客户信息页面

--- src/main/resources/static/customer/searchCustomerInfo.js
+++ src/main/resources/static/customer/searchCustomerInfo.js
@@ -6,37 +6,38 @@ function filterList(customer) {
     return customer.customerName.indexOf($('#searchInput').val()) != -1;
 }
 
-$(function(){
+$(document).ready(function () {
 
     var searchCustInfoVue = new Vue({
-        el:'#searchResult',
-        data:{
+        el: '#customerVue',
+        data: {
+            showPage: ['titleBar', 'searchCustomer', 'customers'],
             searchWord: '',
             errMsg: undefined,
             customerList: []
         },
-        methods:{
-            'showResult':function () {
+        methods: {
+            'showResult': function () {
                 var thisVue = this;
                 $.ajax({
-                    type:'get',
-                    url:'/customer/queryCustomer',
-                    data:{
+                    type: 'get',
+                    url: '/customer/queryCustomer',
+                    data: {
                         searchWord: ''
                     },
-                    dataType:'json',
-                    cache:false
+                    dataType: 'json',
+                    cache: false
                 }).done(function (result) {
-                    if (result.successFlg){
+                    if (result.successFlg) {
                         thisVue.$set(thisVue, 'customerList', result.customerList);
-                    }else {
+                    } else {
                         thisVue.errMsg = result.errMsg;
                     }
                 })
             }
         }
     });
-
+    searchCustInfoVue.showResult();
 
     var $searchBar = $('#searchBar'),
         $searchResult = $('#searchResult'),
@@ -45,57 +46,40 @@ $(function(){
         $searchClear = $('#searchClear'),
         $searchCancel = $('#searchCancel');
 
-    function hideSearchResult(){
+    function hideSearchResult() {
         $searchResult.hide();
         $searchInput.val('');
     }
-    function cancelSearch(){
+
+    function cancelSearch() {
         hideSearchResult();
         $searchBar.removeClass('weui-search-bar_focusing');
         $searchText.show();
     }
 
-    $searchText.on('click', function(){
+    $searchText.on('click', function () {
         $searchBar.addClass('weui-search-bar_focusing');
         $searchInput.focus();
+        $searchResult.show();
     });
     $searchInput
-        .on('keydown', function () {
-            $searchResult.show();
-            searchCustInfoVue.showResult();
-        })
         .on('blur', function () {
-            if(!this.value.length) cancelSearch();
+            if (!this.value.length) cancelSearch();
         })
-        .on('click', function(){
-            $searchResult.show();
-            searchCustInfoVue.showResult();
-        })
+        // .on('click', function () {
+        //     $searchResult.show();
+        //     //searchCustInfoVue.showResult();
+        // })
         .bind('keypress', function (event) {
-            if (event.keyCode == 13){
-
-                $.ajax({
-                    type:'get',
-                    url:'/customer/queryCustomer',
-                    data:{
-                        searchWord: $searchInput.val()
-                    },
-                    dataType:'json',
-                    cache:false
-                }).done(function (result) {
-                    if (result.successFlg){
-                        //$searchResult.customerList = result.customerList;   跳转显示客户信息页面
-                    }else {
-                        this.errMsg = result.errMsg;
-                    }
-                })
+            if (event.keyCode == 13) {
+                //hello
             }
         });
-    $searchClear.on('click', function(){
+    $searchClear.on('click', function () {
         hideSearchResult();
         $searchInput.focus();
     });
-    $searchCancel.on('click', function(){
+    $searchCancel.on('click', function () {
         cancelSearch();
         $searchInput.blur();
     });
