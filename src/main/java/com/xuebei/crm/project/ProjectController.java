37eb75069df2ce1803e2b9682a0e9f5e9379e89f
徐天赐 <1342532052@qq.com>
2018-07-24 17:55:06 +0800

    项目详情查看界面

--- src/main/java/com/xuebei/crm/project/ProjectController.java
+++ src/main/java/com/xuebei/crm/project/ProjectController.java
@@ -1,7 +1,23 @@
 package com.xuebei.crm.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Administrator on 2018/7/24.
  */
+@Controller
+@RequestMapping("/project")
 public class ProjectController {
+
+    //@Autowired
+    //private ProjectService projectService;
+
+    @RequestMapping("/projectDetail")
+    public String detail(){
+        return "projectDetail";
+    }
+
+
 }
