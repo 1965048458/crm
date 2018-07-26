3fa3bebac8f059aca00fc1402ecc923c6682e365
徐天赐 <1342532052@qq.com>
2018-07-26 10:20:12 +0800

    项目详情页面修改

--- src/main/resources/static/project/projectDetail.js
+++ src/main/resources/static/project/projectDetail.js
@@ -1,5 +1,24 @@
 
-$(function(){
+$(document).ready(function(){
+
+    var projectVue = new Vue({
+        el:'#projectVue',
+        data:{
+            records:[]
+        },
+        methods:{
+            'back':function () {
+
+            },
+            'apply':function () {
+                $('#applyDiv').show();
+            },
+            'cancel':function () {
+                $('#applyDiv').hide();
+            }
+        }
+    });
+
     $().timelinr({
         orientation: 'vertical', //垂直滚动
         issuesSpeed: 300, // 对应内容区的滚动速度，可为100～1000之间的数字，或者设置为'slow', 'normal' or 'fast'
@@ -7,4 +26,5 @@ $(function(){
         arrowKeys: 'true', //支持方向键
         startAt: 1 //初始化起点，即初始化轴点位置，数字
     });
+
 });
