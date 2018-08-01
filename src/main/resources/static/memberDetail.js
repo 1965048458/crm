502a739f4b8ef6d24e4d87baee6d631023312625
徐天赐 <1342532052@qq.com>
2018-08-01 14:35:11 +0800

    添加成员详情界面,成员设置

--- src/main/resources/static/memberDetail.js
+++ src/main/resources/static/memberDetail.js
@@ -1,3 +1,38 @@
 /**
  * Created by Administrator on 2018/7/31.
  */
+$(document).ready(function () {
+    var memberDetail = new Vue({
+        el: "#detailVue",
+        data: {
+            showPage: 'mInfo',
+            companyUser: {},
+            deptList:['研发部门','销售部门','财务部门','后勤部门'],
+            department: '',
+            position: ''
+        },
+        methods: {
+            'back': function () {
+                //window.location = "/company/newCompany";
+            },
+            'setting': function () {
+                this.showPage = 'mSetting';
+            },
+            'delete': function () {
+
+            },
+            'update': function () {
+
+            },
+            'backToMain':function () {
+                this.showPage = 'mInfo';
+            },
+            'setDept':function () {
+                this.showPage = 'selDept';
+            },
+            'backToSetting':function () {
+                this.showPage = 'mSetting';
+            }
+        }
+    });
+});
