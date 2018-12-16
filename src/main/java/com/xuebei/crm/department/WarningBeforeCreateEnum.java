f5b68a682500bb9e50cb8de84ddcba5f7d34f25c
wyb <1965048458@qq.com>
2018-12-16 20:29:13 +0800

    修改文字描述

--- src/main/java/com/xuebei/crm/department/WarningBeforeCreateEnum.java
+++ src/main/java/com/xuebei/crm/department/WarningBeforeCreateEnum.java
@@ -4,8 +4,8 @@ package com.xuebei.crm.department;
  * Created by Administrator on 2018/8/20.
  */
 public enum  WarningBeforeCreateEnum {
-    APPLY_BY_ME("您已经申请过或已经圈走了"),
-    APPLY_BY_OTHERS("已经被别人申请了或被别人圈走了"),
+    APPLY_BY_ME("该学院/部门已创建"),
+    APPLY_BY_OTHERS("该学院/部门已创建"),
     NO_ONE_APPLY("还没有任何人提出过申请，可以申请");
 
     private String name;
