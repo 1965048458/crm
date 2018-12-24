b0a488dc02b0e2e8bad283eb3dd0bf0c162527b8
wyb <1965048458@qq.com>
2018-12-24 14:59:54 +0800

    中英文错误

--- src/main/resources/static/loginRegister.js
+++ src/main/resources/static/loginRegister.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function () {
             pwd: '',
             captcha: '',
             showPage: 'telRegister',
-            gender: '男',
+            gender: 'MALE',
             mail: '',
             time: 60,
             flag: true,
